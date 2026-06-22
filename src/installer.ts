ad3c991a97c3e7948badb741ebc02d2da949818f
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-22 14:13:05 +0530

    code linted

--- src/installer.ts
+++ src/installer.ts
@@ -142,11 +142,9 @@ async function getFullVersion(installPath: string) {
 
 function checkCompatibility() {
   // const supported = ["linux-x64", "win32-x64", "darwin-arm64", "darwin-x64"]
-
   // const platform = os.platform()
   // const arch = os.arch()
   // const host = `${platform}-${arch}`
-
   // if (!supported.includes(host)) {
   //   throw new Error(`Unsupported host "${host}"`)
   // }
